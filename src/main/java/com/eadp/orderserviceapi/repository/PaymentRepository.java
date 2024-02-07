97b259b6f801148bff9c3c48be4fdcd86b293330
Helanka Singhapurage <helankas26@gmail.com>
2024-02-07 12:09:17 +0530

    add repos

---
+++ src/main/java/com/eadp/orderserviceapi/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.eadp.orderserviceapi.repository;
+
+import com.eadp.orderserviceapi.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
